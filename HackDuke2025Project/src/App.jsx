94584b0bd444db02f25a459c4ac4ebeacd426250
Cole <cmb256@duke.edu>
2025-02-08 17:00:09 -0500

    V2 css

--- HackDuke2025Project/src/App.jsx
+++ HackDuke2025Project/src/App.jsx
@@ -1,22 +1,22 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
-import Header from './components/Header.jsx';
-import Discover from './components/Discover.jsx';
-import Portfolio from './components/Portfolio.jsx';
-import Feed from './components/Feed.jsx';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Header from "./components/Header.jsx";
+import Discover from "./components/Discover.jsx";
+import Portfolio from "./components/Portfolio.jsx";
+import Feed from "./components/Feed.jsx";
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Discover />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
+      <div className="app-container">
+        <Header />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Discover />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/feed" element={<Feed />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
